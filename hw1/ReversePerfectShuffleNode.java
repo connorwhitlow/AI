d2c23c8eb7cc1f96b0f72bb87d543a5279bd3b5a
Yashaswe Amatya <amatyayasu@gmail.com>
2024-01-31 22:25:09 -0500

    Update ReversePerfectShuffleNode.java

--- hw1/ReversePerfectShuffleNode.java
+++ hw1/ReversePerfectShuffleNode.java
@@ -74,20 +74,20 @@ public class ReversePerfectShuffleNode extends SearchNode {
 	public static void main(String[] args)
 	{
 		ReversePerfectShuffleNode node = new ReversePerfectShuffleNode();
-		System.out.println(node.toString());
-		
-		for(SearchNode c: node.expand())
-		{
-			System.out.println(c.toString());
-		}
+//		System.out.println(node.toString());
+//		
+//		for(SearchNode c: node.expand())
+//		{
+//			System.out.println(c.toString());
+//		}
 		BreadthFirstSearcher bfs = new BreadthFirstSearcher();
 		bfs.search(node);
 		
 		if (bfs.search(node)) {
 			// successful search
-			System.out.println("Goal node found in " + bfs.getNodeCount() 
-			+ " nodes.");
-			System.out.println("Goal path:");
+//			System.out.println("Goal node found in " + bfs.getNodeCount() 
+//			+ " nodes.");
+//			System.out.println("Goal path:");
 			bfs.printGoalPath();
 		} else {
 			// unsuccessful search
