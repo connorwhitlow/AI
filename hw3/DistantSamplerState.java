2456bc3a31a59b59a52f8bf5cc9c4f62edb61c0a
Yashaswe Amatya <amatyayasu@gmail.com>
2024-02-15 11:48:34 -0500

    Add files via upload

--- hw3/DistantSamplerState.java
+++ hw3/DistantSamplerState.java
@@ -1,31 +1,57 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class DistantSamplerState implements State {
+	
+	Random random = new Random();
 	private double[][] data;
 	private int numSamples;
 	private int undoSample1;
 	private int undoSample2;
 	private int[] sampleIndices;
 
-	public DistantSamplerState(double[][] data, int numSamples) {
+
+	public DistantSamplerState(double[][] data, int numSamples)
+
+	{
 
 		this.numSamples = numSamples;
+
+		this.sampleIndices = new int[numSamples];
+
+
+
 		this.data = data;
-		sampleIndices = new int[numSamples];
 
-		ArrayList<Integer> indexList = new ArrayList<>();
-		for (int i = 0; i < data.length; i++) {
-			indexList.add(i);
-		}
-//		System.out.println("Initial indices: " + indexList); // Debug output
 
-		for (int i = 0; i < numSamples; i++) {
-			int randomIndex = (int) (Math.random() * indexList.size());
-			sampleIndices[i] = indexList.get(randomIndex);
-			indexList.remove(randomIndex);
+
+		for (int i = 0 ; i < numSamples ; ++i) {
+		
+			int index = random.nextInt(data.length);
+
+
+
+			while (contains(data[index])) {
+
+				index = random.nextInt(data.length);
+
+			}
+
+			sampleIndices[i] = index;
+
 		}
-//		System.out.println("Sample indices: " + Arrays.toString(sampleIndices)); // Debug output
+
+	}
+
+	public boolean contains(double[] target) {
+		for(int i = 0; i < sampleIndices.length; ++i) {
+			if(data[i] == target) {
+				return true;
+			}
+		}
+
+
+		return false;
 	}
 
 
@@ -35,25 +61,25 @@ public class DistantSamplerState implements State {
 		while (sample == otherSample) {
 			otherSample = (int) (Math.random() * numSamples);
 		}
-	
-			int temp = sampleIndices[sample];
-			sampleIndices[sample] = sampleIndices[otherSample];
-			sampleIndices[otherSample] = temp;
-			undoSample1 = sample;
-			undoSample2 = otherSample;
-	
+
+		int temp = sampleIndices[sample];
+		sampleIndices[sample] = sampleIndices[otherSample];
+		sampleIndices[otherSample] = temp;
+		undoSample1 = sample;
+		undoSample2 = otherSample;
+
 	}
 
 	public void undo() {
-	        int temp = sampleIndices[undoSample1];
-	        sampleIndices[undoSample1] = sampleIndices[undoSample2];
-	        sampleIndices[undoSample2] = temp;
-	
+		int temp = sampleIndices[undoSample1];
+		sampleIndices[undoSample1] = sampleIndices[undoSample2];
+		sampleIndices[undoSample2] = temp;
+
 	}
 
 	public double energy() {
 		double totalEnergy = 0;
-	
+
 		for (int i = 0; i < numSamples - 1; ++i) { 
 			double[] firstPoint = data[sampleIndices[i]];
 			for (int j = i + 1; j < numSamples; ++j) { 
@@ -72,18 +98,18 @@ public class DistantSamplerState implements State {
 
 
 	public DistantSamplerState clone() {
-	    try {
-	        DistantSamplerState clone = (DistantSamplerState) super.clone();
-	        clone.data = new double[data.length][];
-	        clone.sampleIndices = sampleIndices.clone(); // Cloning the sampleIndices array
-	        for (int i = 0; i < data.length; ++i) {
-	            clone.data[i] = data[i].clone();
-	        }
-	        return clone;
-	    } catch (CloneNotSupportedException e) {
-	        e.printStackTrace();
-	    }
-	    return null;
+		try {
+			DistantSamplerState clone = (DistantSamplerState) super.clone();
+			//	        clone.data = new double[data.length][];
+			clone.sampleIndices = sampleIndices.clone(); // Cloning the sampleIndices array
+			//	        for (int i = 0; i < data.length; ++i) {
+			//	            clone.data[i] = data[i].clone();
+			//	        }
+			return clone;
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public int[] getSampleIndices() {
@@ -96,20 +122,20 @@ public class DistantSamplerState implements State {
 	}
 
 	public String toString() {
-	    StringBuilder sb = new StringBuilder();
-	    sb.append("[");
-	    for (int i = 0; i < sampleIndices.length; i++) {
-	        if (i > 0) {
-	            sb.append(", ");
-	        }
-	        int index = sampleIndices[i];
-	        if (index >= 0 && index < data.length) {
-	            sb.append(Arrays.toString(data[index]));
-	        } else {
-	            sb.append("Invalid index" + index +" " + data.length);
-	        }
-	    }
-	    sb.append("]");
-	    return sb.toString();
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (int i = 0; i < sampleIndices.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			int index = sampleIndices[i];
+			if (index >= 0 && index < data.length) {
+				sb.append(Arrays.toString(data[index]));
+			} else {
+				sb.append("Invalid index" + index +" " + data.length);
+			}
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 }
